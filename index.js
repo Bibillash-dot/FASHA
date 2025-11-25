06ba59b17b52984477a9036e9a5638bedd2707c3
Bibillash-dot <sabillashley@gmail.com>
2025-11-25 17:04:10 +0800

    Refactor index.js to use ES6 imports

--- index.js
+++ index.js
@@ -1,38 +1,17 @@
-const {
-    default: makeWASocket,
-    useMultiFileAuthState,
-    fetchLatestBaileysVersion
-} = require("@whiskeysockets/baileys");
-
-const chalk = require("chalk");
-const pino = require("pino");
-
-const pairingNumber = process.argv.includes("--pairing")
-    ? process.argv[process.argv.indexOf("--pairing") + 1]
-    : null;
+import makeWASocket, { useMultiFileAuthState } from "@whiskeysockets/baileys"
+import chalk from "chalk"
 
 async function startBot() {
-    const { state, saveCreds } = await useMultiFileAuthState("./session");
-    const { version } = await fetchLatestBaileysVersion();
-
-    console.log(chalk.blue("Starting FASHA WhatsApp Bot..."));
+    const { state, saveCreds } = await useMultiFileAuthState('./session')
 
     const sock = makeWASocket({
-        logger: pino({ level: "silent" }),
         auth: state,
-        printQRInTerminal: !pairingNumber,
-        mobile: false,
-        version,
-        browser: ["FASHA", "Chrome", "120.0.0"],
-        pairingNumber
-    });
+        printQRInTerminal: true
+    })
 
-    sock.ev.on("creds.update", saveCreds);
+    sock.ev.on('creds.update', saveCreds)
 
-    sock.ev.on("connection.update", ({ connection, lastDisconnect }) => {
-        if (connection === "open") console.log(chalk.green("FASHA BOT connected!"));
-        if (connection === "close") console.log("Connection closed:", lastDisconnect?.error);
-    });
+    console.log(chalk.blueBright("Starting FASHA WhatsApp Bot..."))
 }
 
-startBot();
+startBot()
